ae2012a7c2a64d1ba99ef6128ac0432c399de686
Christof Marti <chrmarti@microsoft.com>
2018-01-09 11:15:45 +0100

    Conditional import

---
+++ service/azservice/tooling.py
@@ -0,0 +1,12 @@
+"""tooling integration"""
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from distutils.version import LooseVersion
+from azure.cli.core import __version__
+if LooseVersion(__version__) < LooseVersion('2.0.24'):
+    from azservice.tooling1 import GLOBAL_ARGUMENTS, initialize, load_command_table, get_help, get_current_subscription, get_configured_defaults, get_defaults, is_required, run_argument_value_completer
+else:
+    from azservice.tooling2 import GLOBAL_ARGUMENTS, initialize, load_command_table, get_help, get_current_subscription, get_configured_defaults, get_defaults, is_required, run_argument_value_completer
