86fb2092e46dd7906e5c9e970e89978dbf818206
mburleigh <matt.burleigh@gmail.com>
2019-06-27 10:45:01 -0400

    use import for elegant-spinner more specific variable names handle running multiple commands simultaneously fix status bar item dispose fix promise chaining

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import { HoverProvider, Hover, SnippetString, StatusBarAlignment, StatusBarItem,
 import { AzService, CompletionKind, Arguments, Status } from './azService';
 import { parse, findNode } from './parser';
 import { exec } from './utils';
+import * as spinner from 'elegant-spinner';
 
 export function activate(context: ExtensionContext) {
     const azService = new AzService(azNotFound);
@@ -145,8 +146,6 @@ class RunLineInTerminal {
     }
 }
 
-const elegantSpinner = require('elegant-spinner');
-
 class RunLineInEditor {
 
     private resultDocument: TextDocument | undefined;
@@ -154,10 +153,11 @@ class RunLineInEditor {
     private queryEnabled = false;
     private query: string | undefined;
     private disposables: Disposable[] = [];
-    private runStatusBarItem: StatusBarItem;
-    private interval!: NodeJS.Timer;
-    private spinner = elegantSpinner();
+    private commandRunningStatusBarItem: StatusBarItem;
+    private statusBarUpdateInterval!: NodeJS.Timer;
+    private statusBarSpinner = spinner();
     private hideStatusBarItemTimeout! : NodeJS.Timeout;
+    private statusBarItemText : string = '';
 
     constructor(private status: StatusBarInfo) {
         this.disposables.push(commands.registerTextEditorCommand('ms-azurecli.toggleLiveQuery', editor => this.toggleQuery(editor)));
@@ -165,15 +165,29 @@ class RunLineInEditor {
         this.disposables.push(workspace.onDidCloseTextDocument(document => this.close(document)));
         this.disposables.push(workspace.onDidChangeTextDocument(event => this.change(event)));
 
-        this.runStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        this.commandRunningStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        this.disposables.push(this.commandRunningStatusBarItem);
     }
 
+    private runningCommandCount : number = 0;
     private run(source: TextEditor) {
+        this.runningCommandCount += 1;
         const t0 = Date.now();
-        this.interval = setInterval(() => {
-            this.runStatusBarItem.text = `Waiting for response ${this.spinner()}`;
-        }, 50);
-        this.runStatusBarItem.show();
+        if (this.runningCommandCount == 1)
+        {
+            this.statusBarItemText = `Waiting for response`;
+            this.statusBarUpdateInterval = setInterval(() => {
+                if (this.runningCommandCount == 1)
+                {
+                    this.commandRunningStatusBarItem.text = `${this.statusBarItemText} ${this.statusBarSpinner()}`;
+                }
+                else
+                {
+                    this.commandRunningStatusBarItem.text = `${this.statusBarItemText} [${this.runningCommandCount}] ${this.statusBarSpinner()}`;
+                }
+            }, 50);
+        }
+        this.commandRunningStatusBarItem.show();
         clearTimeout(this.hideStatusBarItemTimeout);
 
         this.parsedResult = undefined;
@@ -185,22 +199,28 @@ class RunLineInEditor {
             .then(target => replaceContent(target, JSON.stringify({ 'Running command': line }) + '\n')
                 .then(() => exec(line))
                 .then(({ stdout }) => stdout, ({ stdout, stderr }) => JSON.stringify({ stderr, stdout }, null, '    '))
-                .then(content => {
-                    replaceContent(target, content)
-                        .then(() => this.parsedResult = JSON.parse(content))
-                        .then(undefined, err => {});
-                    clearInterval(this.interval);
-                    this.runStatusBarItem.text = 'AZ CLI command executed in ' + (Date.now() - t0) + ' milliseconds.';
-                })
-                .then(() => this.hideStatusBarItem())
+                .then(content => replaceContent(target, content)
+                    .then(() => this.parsedResult = JSON.parse(content))
+                    .then(undefined, err => {})
+                )
+                .then(() => this.commandFinished(t0))
             )
             .then(undefined, console.error);
     }
 
-    private hideStatusBarItem()
+    private commandFinished(startTime: number)
     {
-        // hide status bar item after 10 seconds to keep status bar uncluttered
-        this.hideStatusBarItemTimeout = setTimeout(() => this.runStatusBarItem.hide(), 10000)
+        this.runningCommandCount -= 1
+        this.statusBarItemText = 'AZ CLI command executed in ' + (Date.now() - startTime) + ' milliseconds';
+        this.commandRunningStatusBarItem.text = this.statusBarItemText;
+
+        if (this.runningCommandCount == 0)
+        {
+            clearInterval(this.statusBarUpdateInterval);
+
+            // hide status bar item after 10 seconds to keep status bar uncluttered
+            this.hideStatusBarItemTimeout = setTimeout(() => this.commandRunningStatusBarItem.hide(), 10000);
+        }
     }
 
     private toggleQuery(source: TextEditor) {
@@ -265,7 +285,6 @@ class RunLineInEditor {
 
     dispose() {
         this.disposables.forEach(disposable => disposable.dispose());
-        this.runStatusBarItem.dispose();
     }
 }
 
